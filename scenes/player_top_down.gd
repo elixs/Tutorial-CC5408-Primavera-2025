ec6d381c4f315b1fd1fd52482b9d0d30e1db1a3c
Elías Zelada <elixs.777@gmail.com>
2025-10-17 11:54:21 -0300

    more topdown attacks, and particles

--- scenes/player_top_down.gd
+++ scenes/player_top_down.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
-@onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
+@onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/movement/playback"]
 @onready var pivot: Node2D = $Pivot
 
 
@@ -15,8 +15,23 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	
 	var move_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if animation_tree["parameters/attack/active"]:
+		move_input = Vector2.ZERO
+	
+	
 	velocity = velocity.move_toward(move_input * max_speed, acceleration * delta)
+	
+	
+	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		var attack_direction = global_position.direction_to(get_global_mouse_position())
+		animation_tree["parameters/attack_blend_space/blend_position"] = attack_direction
+		animation_tree["parameters/attack/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+		move_input = Vector2.ZERO
+		pivot.scale.x = sign(attack_direction.x)
 	
 	
 	# animation
@@ -30,8 +45,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		playback.travel("idle")
 	
-	animation_tree["parameters/run/time_scale/scale"] = speed / (max_speed * 0.5)
-	
+	animation_tree["parameters/movement/run/time_scale/scale"] = speed / (max_speed * 0.5)
 
 func take_damage(_damage):
 	Debug.log("Auch player")
